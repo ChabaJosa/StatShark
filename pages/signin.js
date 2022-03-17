2aa8c51ea9a6b918dd31f8d6e48e3e50df4b9214
ChabaJosa <chabagjg@gmail.com>
2022-03-16 22:23:56 -0400

    Added sign in page

---
+++ pages/signin.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '../styles/Signin.module.css'
+
+export default function signin(){
+  return (
+    <div className={styles.container}>signin</div>
+  )
+}
