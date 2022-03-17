2409d2ede9b9f5cae00e3de569d884e064a8c92f
alfredorca <alfredorca@gmail.com>
2022-03-16 23:17:32 -0400

    Start style prototype

--- pages/signin.js
+++ pages/signin.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import styles from '../styles/Signin.module.css'
+import { Container } from 'semantic-ui-react'
+
 
 export default function signin(){
   return (
-    <div className={styles.container}>signin</div>
+    <div className={styles.container}>
+    <Container>
+    <p>
+      Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+      ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
+      magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
+      ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
+      quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
+      arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
+      Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
+      dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+      Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
+      Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
+      viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
+      Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
+    </p>
+  </Container>
+  </div>
   )
 }
